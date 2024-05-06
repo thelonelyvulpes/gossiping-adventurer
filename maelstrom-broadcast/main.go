a0ad35ee610e21ff8795030c591d80562b902c02
grant.lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2024-05-06 17:32:55 +0100

    broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -4,17 +4,27 @@ import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	n := maelstrom.NewNode()
 
+	var idx *uint64
+	var broadcastVal *int
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		// Unmarshal the message req as an loosely-typed map.
 		var req map[string]any
 		if err := json.Unmarshal(msg.Body, &req); err != nil {
 			return err
 		}
+		r := body["message"].(string)
+		val, err := strconv.Atoi(r)
+		if err != nil {
+			panic(err)
+		}
+		*broadcastVal = val
 
 		// Update the message type to return back.
 		req["type"] = "broadcast_ok"
@@ -35,6 +45,7 @@ func main() {
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, req)
 	})
+
 	n.Handle("topology", func(msg maelstrom.Message) error {
 		// Unmarshal the message req as an loosely-typed map.
 		var req map[string]any
@@ -49,6 +60,7 @@ func main() {
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, req)
 	})
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
--- maelstrom-uniq-id/main.go
+++ maelstrom-uniq-id/main.go
@@ -11,13 +11,13 @@ const NodeValueSpace = 1000000000000
 
 func main() {
 	n := maelstrom.NewNode()
-	idx := make([]uint64, 1)
-	
+	var idx *uint64
+
 	n.Handle("generate", func(msg maelstrom.Message) error {
-		if idx[0] == 0 {
+		if *idx == 0 {
 			for i := 0; i < len(n.NodeIDs()); i++ {
 				if strings.Compare(n.ID(), n.NodeIDs()[i]) == 0 {
-					idx[0] = uint64(i + 1) * NodeValueSpace
+					*idx = uint64(i+1) * NodeValueSpace
 					break
 				}
 			}
@@ -31,9 +31,9 @@ func main() {
 		
 		res := make(map[string]any, 2)
 		// Update the message type to return back.
-		res["type"] = "generate_ok"
-		res["id"] = idx[0]
-		idx[0]++
+		body["type"] = "generate_ok"
+		body["id"] = *idx
+		*idx++
 
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, res)
